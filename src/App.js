3361fef56455fa3938cfdc9496df3bc780bd40ca
Kimmy <kimberlyparrenas@gmail.com>
2022-06-22 20:21:33 +0800

    Conditionals in JSX

--- src/App.js
+++ src/App.js
@@ -1,6 +1,37 @@
 function App(){
+    const title='Blog Post'
+    const body ='This is my blog post'
+
+    const comments = [
+        {id: 1, text: 'Comment One'},
+        {id: 2, text: 'Comment two'},
+        {id: 3, text: 'Comment three'},
+    ]
+
+    const loading = false
+    const showComments = true
+
+    if(loading) return <h1>Loading...</h1>
+
+    const commentBlock = (
+        <div className="comments">
+            <h3>Comments ({comments.length})</h3>
+            <ul>
+                {comments.map((comment, index) => (
+                    <li key={index}>{comment.text}</li>
+                ))}
+            </ul>
+        </div>
+    )
+
     return (
-        <h1>Hello from the app component</h1>
+        <div className="container">
+            <h1>{title}</h1>
+            <p>{body}</p>
+
+            {showComments && commentBlock}
+
+        </div>
     )
 }
 
