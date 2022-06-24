b8b301acb349bcb8c9a31e9062e5d551eb24969b
Kimmy <kimberlyparrenas@gmail.com>
2022-06-24 19:50:04 +0800

    Managing Global State

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
+import { useState } from "react"
 import Header from "./components/Header"
-import FeedbackItem from "./components/FeedbackItem"
+import FeedbackList from "./components/FeedbackList"
+import FeedbackData from "./data/FeedbackData";
 
 function App(){
 
+    const [feedback, setFeedback] = useState(FeedbackData);
+
     return (
         <>
-            {/* <Header bgColor='red' textColor ='blue'/> */}
             <Header/>
             <div className="container">
-                <FeedbackItem/>
+                <FeedbackList feedback = {feedback}/>
             </div>
         </>
     )
---
+++ src/components/FeedbackList.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import FeedbackItem from './FeedbackItem'
+
+function FeedbackList({feedback}) {
+    console.log(feedback)
+
+    if(!feedback || feedback.lenght === 0){
+        return <p>No Feedback Yet</p>
+    }
+    
+    return (
+        <div className='feedback-list'>
+            {feedback.map((item) => (
+                <FeedbackItem key={item.id} item={item}/>
+            ))}
+        </div>
+    )
+}
+
+export default FeedbackList
