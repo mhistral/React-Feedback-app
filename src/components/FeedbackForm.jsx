d2fde6a64f7ac3fb0bede5668c68dc0b4c1bb357
Kimmy <kimberlyparrenas@gmail.com>
2022-06-25 08:08:35 +0800

    Realtime Validation

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -6,7 +6,20 @@ import Button from './shared/Button'
 function FeedbackForm() {
 
     const [text, setText] = useState('')
+    const [btnDisabled, setBtnDisabled] = useState(true)
+    const [message, setMessage] = useState('')
+
     const handleTextChange = (e) => {
+        if(text === ''){
+            setBtnDisabled(true);
+            setMessage(null)
+        }else if(text !== '' && text.trim().length <= 10){
+            setBtnDisabled(true);
+            setMessage('Text must be atleast 10 Characters')
+        }else{
+            setBtnDisabled(false);
+            setMessage(null)
+        }
         setText(e.target.value)
     }
 
@@ -17,8 +30,9 @@ function FeedbackForm() {
                 {/* @todo - Rating Select Component */}
                 <div className="input-group">
                     <input onChange={handleTextChange} value={text} type="text" placeholder='Write a Review'/>
-                    <Button type="submit">Send</Button>
+                    <Button type="submit" isDisabled={btnDisabled}>Send</Button>
                 </div>
+                {message && <div className="message">{message}</div> }
             </form>
         </Card>
     )
