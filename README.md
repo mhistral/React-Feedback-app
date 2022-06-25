c1c550b90320188c4c3a68f20d32956e8d40f86d
Kimmy <kimberlyparrenas@gmail.com>
2022-06-25 09:42:14 +0800

    Fade Animation with Framer motion

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ run:
 ### `npm i react-icon`
 
 ### `npm i uuid`
+
+### `npm i framer-motion@4.1.17`  
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,34 +1,57 @@
-import React from 'react'
-import FeedbackItem from './FeedbackItem'
-import PropTypes from 'prop-types'
+import { motion, AnimatePresence } from "framer-motion";
+import React from "react";
+import FeedbackItem from "./FeedbackItem";
+import PropTypes from "prop-types";
 
-function FeedbackList({feedback, handleDelete}) {
-    console.log(feedback)
+function FeedbackList({ feedback, handleDelete }) {
+	console.log(feedback);
 
-    if(!feedback || feedback.lenght === 0){
-        return <p>No Feedback Yet</p>
-    }
-    
-    return (
-        <div className='feedback-list'>
-            {feedback.map((item) => (
-                <FeedbackItem 
-                    key={item.id} 
-                    item={item} 
-                    handleDelete={handleDelete}/>
-            ))}
-        </div>
-    )
+	if (!feedback || feedback.lenght === 0) {
+		return <p>No Feedback Yet</p>;
+	}
+
+	return (
+		<div className="feedback-list">
+			<AnimatePresence>
+				{feedback.map((item) => (
+					<motion.div 
+                  key={item.id}
+                  initial={{ opacity:0 }}
+                  animate={{ opacity:1 }}
+                  exit={{ opacity:0 }}
+               >
+						<FeedbackItem
+							key={item.id}
+							item={item}
+							handleDelete={handleDelete}
+						/>
+					</motion.div>
+				))}
+			</AnimatePresence>
+		</div>
+	);
+
+	// version without the animation
+	//  return (
+	//      <div className='feedback-list'>
+	//          {feedback.map((item) => (
+	//              <FeedbackItem
+	//                  key={item.id}
+	//                  item={item}
+	//                  handleDelete={handleDelete}/>
+	//          ))}
+	//      </div>
+	//  )
 }
 
 FeedbackList.propType = {
-    feedback: PropTypes.arrayOf(
-        PropTypes.shape({
-            id: PropTypes.number.isRequired,
-            text: PropTypes.string.isRequired,
-            rating: PropTypes.number.isRequired
-        })
-    )
-}
+	feedback: PropTypes.arrayOf(
+		PropTypes.shape({
+			id: PropTypes.number.isRequired,
+			text: PropTypes.string.isRequired,
+			rating: PropTypes.number.isRequired,
+		})
+	),
+};
 
-export default FeedbackList
+export default FeedbackList;
