4caaad59d0c9e6ce07d3c981e97f3fb0b1af809c
Kimmy <kimberlyparrenas@gmail.com>
2022-06-26 15:12:43 +0800

    Fetech data from JSON-Server Backend

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,66 +1,66 @@
-import { v4 as uuidv4 } from "uuid";
-import React, { createContext, useState} from "react";
+import { v4 as uuidv4 } from "uuid"
+import React, { createContext, useState, useEffect } from "react"
 
-const FeedbackContext = createContext();
+const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({ children }) => {
-	const [feedback, setFeedback] = useState([
-		{
-			id: 1,
-			text: "This is Feedback item 1",
-			rating: 10,
-		},
-		{
-			id: 2,
-			text: "This is Feedback item 2",
-			rating: 9,
-		},
-		{
-			id: 3,
-			text: "This is Feedback item 2",
-			rating: 7,
-		},
-	]);
+	const [isLoading, setIsLoading] = useState(true)
+	const [feedback, setFeedback] = useState([])
 
 	const [feedbackEdit, setFeedbackEdit] = useState({
 		item: {},
 		edit: false,
-	});
+	})
+
+	useEffect(() => {
+		fetchFeedback()
+	}, [])
+
+	// Fetch Feedback
+	const fetchFeedback = async () => {
+		const response = await fetch(
+			`http://localhost:5000/feedback?_sort=id&_order=desc`
+		)
+		const data = await response.json()
+
+		setFeedback(data)
+      setIsLoading(false);
+	}
 
 	//Update Feedback Item
 	const updateFeedback = (id, updItem) => {
 		setFeedback(
-			feedback.map((item) => 
-				item.id === id ? { ...item, ...updItem } : item
-		   ))
-	};
+			feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item))
+		)
+	}
 
 	// Delete feedback
 	const deleteFeedback = (id) => {
 		if (window.confirm("Are you Sure you want to Delete?")) {
-			setFeedback(feedback.filter((item) => item.id !== id));
+			setFeedback(feedback.filter((item) => item.id !== id))
 		}
-	};
+	}
 
 	// Add feedback
 	const addFeedback = (newFeedback) => {
-		newFeedback.id = uuidv4();
-		setFeedback([newFeedback, ...feedback]);
-	};
+		newFeedback.id = uuidv4()
+		setFeedback([newFeedback, ...feedback])
+	}
 
 	// Set item to be updated
 	const editFeedback = (item) => {
 		setFeedbackEdit({
 			item,
 			edit: true,
-		});
-	};
+		})
+	}
 
 	return (
 		<FeedbackContext.Provider
 			value={{
 				feedback,
 				feedbackEdit,
+            isLoading,
 				deleteFeedback,
 				addFeedback,
 				editFeedback,
@@ -70,7 +70,7 @@ export const FeedbackProvider = ({ children }) => {
 			{" "}
 			{children}{" "}
 		</FeedbackContext.Provider>
-	);
-};
+	)
+}
 
-export default FeedbackContext;
+export default FeedbackContext
